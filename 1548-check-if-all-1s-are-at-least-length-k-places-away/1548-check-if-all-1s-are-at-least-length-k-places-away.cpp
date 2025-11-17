41f249bbd9b75aac22cb4ac332272dce6087cb85
Dhyey Shah <103097959+Dhyey-19@users.noreply.github.com>
2025-11-17 13:51:29 +0530

    Time: 0 ms (100%), Space: 61.5 MB (47.64%) - LeetHub

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.cpp
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.cpp
@@ -1,15 +1,35 @@
 class Solution {
 public:
     bool kLengthApart(vector<int>& nums, int k) {
-        int prevOne = -1;
-        for(int i = 0; i < nums.size(); i++) {
+        // int prevOne = -1;
+        // for(int i = 0; i < nums.size(); i++) {
+        //     if(nums[i] == 1) {
+        //         if(prevOne != -1) {
+        //             if(i - prevOne - 1 < k) {
+        //                 return false;
+        //             }
+        //         }
+        //         prevOne = i;
+        //     }
+        // }
+        // return true;
+
+        int val = 0;
+        int i = 0;
+        while(i < nums.size()) {
             if(nums[i] == 1) {
-                if(prevOne != -1) {
-                    if(i - prevOne - 1 < k) {
-                        return false;
-                    }
+                break;
+            }
+            i++;
+        }
+        for(int j = i + 1; j < nums.size(); j++) {
+            if(nums[j] == 1) {
+                if(val < k) {
+                    return false;
                 }
-                prevOne = i;
+                val = 0;
+            } else {
+                val++;
             }
         }
         return true;
