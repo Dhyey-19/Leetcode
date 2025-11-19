49986c692948695b2587b0945409c65d3683e07f
Dhyey Shah <103097959+Dhyey-19@users.noreply.github.com>
2025-11-20 01:51:13 +0530

    Time: 1 ms (86.49%), Space: 53.1 MB (57.55%) - LeetHub

---
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int n = nums.size();
+
+        // Mark every seen number by making nums[x-1] negative
+        for (int i = 0; i < n; i++) {
+            int idx = abs(nums[i]) - 1;
+            if (nums[idx] > 0) {
+                nums[idx] = -nums[idx];
+            }
+        }
+
+        // Now collect all indices that were never marked
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                ans.push_back(i + 1);
+            }
+        }
+
+        return ans;    
+    }
+};
