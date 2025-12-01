a6f962dc065597a45613e4e272e89947ea6369f0
Dhyey Shah <103097959+Dhyey-19@users.noreply.github.com>
2025-12-01 22:31:52 +0530

    Time: 15 ms (97.58%), Space: 70.8 MB (95.55%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int, int> freq;
-        for(int num : nums) {
-            freq[num]++;
-        }
-        for(auto& [num, f] : freq) {
-            if(f > 1) {
+        // unordered_map<int, int> freq;
+        // for(int num : nums) {
+        //     freq[num]++;
+        // }
+        // for(auto& [num, f] : freq) {
+        //     if(f > 1) {
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size() - 1; i++) {
+            if(nums[i] == nums[i + 1]) {
                 return true;
             }
         }
