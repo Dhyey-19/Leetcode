b949b2e5e6d38d2162eaca86cb89da6719890f14
Dhyey Shah <103097959+Dhyey-19@users.noreply.github.com>
2025-12-05 20:38:25 +0530

    Time: 0 ms (100%), Space: 10.4 MB (64.03%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -1,24 +1,38 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
+        // Using Queue
+
+        // int time = 0;
+        // queue<pair<int, int>> q; // {index, tickets_needed}
+        // for (int i = 0; i < tickets.size(); ++i) {
+        //     q.push({i, tickets[i]});
+        // }
+        // int idx = 0, val = 0;
+        // while (true) {
+        //     idx = q.front().first;
+        //     val = q.front().second;
+        //     val--;
+        //     q.pop();
+        //     time++;
+        //     if (val) {
+        //         q.push({idx, val});
+        //         continue;
+        //     }
+        //     if(idx == k) {
+        //         break;
+        //     }
+        // }
+        // return time;
+
+        // Optimal - Using Array
+        int target = tickets[k];
         int time = 0;
-        queue<pair<int, int>> q; // {index, tickets_needed}
-        for (int i = 0; i < tickets.size(); ++i) {
-            q.push({i, tickets[i]});
-        }
-        int idx = 0, val = 0;
-        while (true) {
-            idx = q.front().first;
-            val = q.front().second;
-            val--;
-            q.pop();
-            time++;
-            if (val) {
-                q.push({idx, val});
-                continue;
-            }
-            if(idx == k) {
-                break;
+        for(int i = 0; i < tickets.size(); i++) {
+            if(i <= k) {
+                time += min(tickets[i], target);
+            } else {
+                time += min(tickets[i], target - 1);
             }
         }
         return time;
