26f458e1ae7395435770784ee5f582f7ab17b93c
Dhyey Shah <103097959+Dhyey-19@users.noreply.github.com>
2025-12-09 17:22:16 +0530

    Time: 886 ms (48.9%), Space: 428 MB (36.86%) - LeetHub

--- 3583-count-special-triplets/3583-count-special-triplets.cpp
+++ 3583-count-special-triplets/3583-count-special-triplets.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
     int specialTriplets(vector<int>& nums) {
-        const int MOD = 1e9 + 7;
-        unordered_map<int, long long> right;
-        unordered_map<int, long long> left;
-
-        // initially all freq in right map
-        for (int x : nums) right[x]++;
-
+        const int mod = 1e9 + 7;
+        unordered_map<int, int> suffix;
+        unordered_map<int, int> prefix;
+        for(auto x : nums) suffix[x] += 1;
         long long ans = 0;
-
-        for (int j = 0; j < nums.size(); j++) {
-            int mid = nums[j];
-            right[mid]--;  // mid moves from right to left
-
-            long long target = mid * 2;
-
-            // count i < j and k > j
-            long long cnt_i = left[target];
-            long long cnt_k = right[target];
-
-            ans = (ans + (cnt_i * cnt_k) % MOD) % MOD;
-
-            left[mid]++;  // mark mid as processed in left side
+        for(auto x : nums){
+            suffix[x] -= 1;
+            ans += (long long)prefix[2 * x] * suffix[2 * x];
+            prefix[x] += 1;
         }
-
-        return ans;
+        return ans % mod;
     }
 };
