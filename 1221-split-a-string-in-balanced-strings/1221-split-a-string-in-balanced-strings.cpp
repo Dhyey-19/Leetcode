b4b0377f1435013717170f2092fb340ee3133d0c
Dhyey Shah <103097959+Dhyey-19@users.noreply.github.com>
2025-12-10 21:17:26 +0530

    Time: 0 ms (100%), Space: 8.4 MB (15.17%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int balancedStringSplit(string s) {
-        int rcnt = 0, lcnt = 0, cnt = 0;
+        int res = 0, cnt = 0;
         for(char ch : s) {
             if(ch == 'L') {
-                lcnt++;
-            } else {
-                rcnt++;
-            }
-            if(lcnt == rcnt) {
                 cnt++;
-                lcnt = 0;
-                rcnt = 0;
+            } else {
+                cnt--;
+            }
+            if(cnt == 0) {
+                res++;
             }
         }    
-        return cnt;
+        return res;
     }
 };
