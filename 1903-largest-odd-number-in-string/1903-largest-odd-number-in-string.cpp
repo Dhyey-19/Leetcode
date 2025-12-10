e3fd079440a31aca896a805b7922419913353635
Dhyey Shah <103097959+Dhyey-19@users.noreply.github.com>
2025-12-10 21:23:29 +0530

    Time: 2 ms (36.94%), Space: 17.8 MB (36.59%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -6,13 +6,9 @@ public:
         for(int i = num.size() - 1; i>= 0; i--) {
             int digit = num[i] - '0';
             if(digit % 2 == 1) {
-                idx = i;
-                break;
+                return num.substr(0, i + 1);
             }
         }
-        for(int i = 0; i <= idx; i++) {
-            res += num[i];
-        }
-        return res;
+        return "";
     }
 };
