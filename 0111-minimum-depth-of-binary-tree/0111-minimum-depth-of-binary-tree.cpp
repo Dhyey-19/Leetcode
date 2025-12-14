7f638f35b7ea178864c328b8cc5e5ac30fbe3755
Dhyey Shah <103097959+Dhyey-19@users.noreply.github.com>
2025-12-14 13:43:48 +0530

    Time: 0 ms (100%), Space: 146.8 MB (45.4%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -6,15 +6,42 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-        if(!root) return 0;
-        if(!root->left) return 1 + minDepth(root->right);
-        if(!root->right) return 1 + minDepth(root->left);
-        return 1 + min(minDepth(root->left), minDepth(root->right));
+        // Recursive Approach
+
+        // if(!root) return 0;
+        // if(!root->left) return 1 + minDepth(root->right);
+        // if(!root->right) return 1 + minDepth(root->left);
+        // return 1 + min(minDepth(root->left), minDepth(root->right));
+
+        // Iterative Approach
+
+        if (root == nullptr)
+            return 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        int depth = 0;
+        while (!q.empty()) {
+            int levelSize = q.size();
+            depth++;
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                if (node->left == nullptr && node->right == nullptr) {
+                    return depth;
+                }
+                if (node->left != nullptr)
+                    q.push(node->left);
+                if (node->right != nullptr)
+                    q.push(node->right);
+            }
+        }
+        return depth;
     }
 };
