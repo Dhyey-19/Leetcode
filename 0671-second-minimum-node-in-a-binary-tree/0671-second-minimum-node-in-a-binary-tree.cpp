8e1f3d0c0240b188976190f9dc7ec070b164a27b
Dhyey Shah <103097959+Dhyey-19@users.noreply.github.com>
2025-12-21 08:50:59 +0530

    Time: 0 ms (100%), Space: 9.4 MB (15.86%) - LeetHub

---
+++ 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int rootVal;
+    long ans=LONG_MAX;
+    void solve(TreeNode* root){
+        if(!root) return;
+
+        if(root->val>rootVal && root->val<ans){
+            ans=root->val;
+        } else if(rootVal==root->val){
+            solve(root->left);
+            solve(root->right);
+        }
+    }
+    int findSecondMinimumValue(TreeNode* root) {
+        rootVal=root->val;
+        solve(root);
+        return ans==LONG_MAX?-1:(int)ans;
+    }
+};
