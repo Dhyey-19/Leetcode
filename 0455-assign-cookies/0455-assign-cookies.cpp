97efb6ff9a47911dc289def654930c49ca473792
Dhyey Shah <103097959+Dhyey-19@users.noreply.github.com>
2026-01-09 21:16:58 +0530

    Time: 16 ms (11.23%), Space: 44.7 MB (75.72%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int n = size(g), p =size(s);
-        sort(g.begin(),g.end());
-        sort(s.begin(),s.end());
-        int gi = 0, si = 0;
-        while(gi<n && si<p){
-            if(s[si]>=g[gi]){
-                gi++;
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int i = 0, j = 0;
+        int cnt = 0;
+
+        while (i < g.size() && j < s.size()) {
+            if (s[j] >= g[i]) {
+                cnt++;     // child is satisfied
+                i++;       // next child
+                j++;       // next cookie
+            } else {
+                j++;       // cookie too small, try bigger one
             }
-            si++;
         }
-        return gi;
+        return cnt;
     }
 };
