22969d2c563b4466e6fb69aeb8562ef0de25066e
SHAH DHYEY PARAS <103097959+Dhyey-19@users.noreply.github.com>
2026-03-09 16:52:04 +0530

    Time: 4 ms (29.36%), Space: 10.6 MB (83.53%) - LeetHub

--- 0482-license-key-formatting/0482-license-key-formatting.cpp
+++ 0482-license-key-formatting/0482-license-key-formatting.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
     string licenseKeyFormatting(string s, int k) {
-        string str = "";
-        for(char ch : s) {
-            if(ch == '-') {
-                continue;
-            } 
-            if(isdigit(ch)){
-                str += ch;
-            } else {
-                str += toupper(ch);
-            }
-        }
-        string ans = "";
-        int len = str.length();
-        for(int i = len - 1; i >= 0; i-=k) {
-            int j = i, cnt = k;
-            while(j >= 0 && cnt--) {
-                ans += str[j--];
-            }
-            ans += '-';
-        }
-        if(!ans.empty()) {
-            ans.pop_back();
-        }
-        reverse(ans.begin(), ans.end());
-        return ans;
+        string res;
+        int count = 0;
 
+        for (int i = s.size() - 1; i >= 0; i--) {
+            if (s[i] == '-') continue;
+
+            if (count == k) {
+                res += '-';
+                count = 0;
+            }
+
+            res += toupper(s[i]);
+            count++;
+        }
+
+        reverse(res.begin(), res.end());
+        return res;
     }
 };
