2625c6ba7f4e8789ba2b7c9d90b0970176baf10c
SHAH DHYEY PARAS <103097959+Dhyey-19@users.noreply.github.com>
2026-03-12 12:15:53 +0530

    Time: 0 ms (100%), Space: 8.3 MB (10.51%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
 
     int fibo( int n , vector<int>& dp ){
-        if( n == 1 ) return 1;
-        if( n == 0 ) return 0;
-        if( dp[n] != -1 ) return dp[n];
-        dp[n] = fibo( n-1 , dp ) + fibo( n-2 , dp );
+        if(n == 1) return 1;
+        if(n == 0) return 0;
+        if(dp[n] != -1) return dp[n];
+        dp[n] = fibo(n-1, dp) + fibo(n-2, dp);
         return dp[n];
     }
 
     int fib(int n) {
-        vector<int> dp( n+1 , -1 );
-        return fibo(n , dp );
+        vector<int> dp(n+1, -1);
+        return fibo(n, dp);
     }
 };
