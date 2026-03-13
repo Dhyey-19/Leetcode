41b57ca85d6080aefc1a1e0ff73af3b8a1ae8ef2
SHAH DHYEY PARAS <103097959+Dhyey-19@users.noreply.github.com>
2026-03-13 15:30:02 +0530

    Time: 0 ms (100%), Space: 15 MB (98.09%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
-        if(mat.size() * mat[0].size() != r * c) {
-            return mat;
-        }
+        // if(mat.size() * mat[0].size() != r * c) {
+        //     return mat;
+        // }
+        // vector<vector<int>> res(r, vector<int>(c));
+        // int row = 0, col = 0;
+        // for(int i = 0; i < mat.size(); i++) {
+        //     for(int j = 0; j < mat[0].size(); j++) {
+        //         if(col == c) {
+        //             row++;
+        //             col = 0;
+        //         }
+        //         res[row][col++] = mat[i][j];
+        //     }
+        // }
+        // return res;
+
+        // Optimal
+        int m = mat.size(), n = mat[0].size();
+        if(m * n != r * c) return mat;
         vector<vector<int>> res(r, vector<int>(c));
-        int row = 0, col = 0;
-        for(int i = 0; i < mat.size(); i++) {
-            for(int j = 0; j < mat[0].size(); j++) {
-                if(col == c) {
-                    row++;
-                    col = 0;
-                }
-                res[row][col++] = mat[i][j];
-            }
+        for(int i = 0; i < m * n; i++) {
+            res[i / c][i % c] = mat[i / n][i % n];
         }
         return res;
     }
