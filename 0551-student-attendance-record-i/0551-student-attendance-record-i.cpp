ce473edc4a56e655418f6a2967fd708ed7fb4dca
SHAH DHYEY PARAS <103097959+Dhyey-19@users.noreply.github.com>
2026-03-13 15:10:47 +0530

    Time: 0 ms (100%), Space: 8.3 MB (50.83%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     bool checkRecord(string s) {
-        int absent = 0, leave = 0;
-        for(int i = 0; i < s.length(); i++) { 
-            if(s[i] == 'P') {
-                continue;
-            } else if(s[i] == 'A') {
+        int absent = 0, late = 0;
+        for(char c : s) {
+            if(c == 'A') {
                 absent++;
-            } else if(s[i] == 'L') {
-                if(i > 0 && s[i - 1] == 'L') {
-                    leave++;
-                } else {
-                    leave = 1;
-                }
+                if(absent >= 2) return false;
             }
-            if(absent >= 2) {
-                return false;
+            if(c == 'L') {
+                late++;
+                if(late >= 3) return false;
+            } else {
+                late = 0;
             }
-            if(leave >= 3) {
-                return false;
-            }
-        }    
+        }
         return true;
     }
 };
