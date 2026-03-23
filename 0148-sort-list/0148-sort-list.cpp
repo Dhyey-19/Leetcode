4e67ba23e976365da698ad9128d04847984240bf
SHAH DHYEY PARAS <103097959+Dhyey-19@users.noreply.github.com>
2026-03-23 11:33:49 +0530

    Time: 3 ms (99.57%), Space: 58.2 MB (57.21%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -1,47 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* findMiddle(ListNode* head) {
-        ListNode* slow = head;
-        ListNode* fast = head->next; // important
-
-        while (fast && fast->next) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        return slow;
-    }
-
-    ListNode* mergeTwoSortedLL(ListNode* list1, ListNode* list2) {
-        ListNode* dummy = new ListNode(-1);
-        ListNode* temp = dummy;
-
-        while (list1 && list2) {
-            if (list1->val <= list2->val) {
-                temp->next = list1;
-                list1 = list1->next;
-            } else {
-                temp->next = list2;
-                list2 = list2->next;
-            }
+    ListNode* sortList(ListNode* head) {
+        vector<int> arr;
+        ListNode* temp = head;
+        while(temp) {
+            arr.push_back(temp->val);
             temp = temp->next;
         }
-
-        if (list1) temp->next = list1;
-        else temp->next = list2;
-
-        return dummy->next;
-    }
-
-    ListNode* sortList(ListNode* head) {
-        if (!head || !head->next) return head;
-
-        ListNode* middle = findMiddle(head);
-        ListNode* right = middle->next;
-        middle->next = nullptr;
-
-        ListNode* left = sortList(head);
-        right = sortList(right);
-
-        return mergeTwoSortedLL(left, right);
+        sort(arr.begin(), arr.end());
+        temp = head;
+        for(int a : arr) {
+            temp->val = a;
+            temp = temp->next;
+        }
+        return head;
     }
 };
