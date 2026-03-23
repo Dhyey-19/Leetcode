9d1f3116385d40a791e904349ad9aec791c4a664
SHAH DHYEY PARAS <103097959+Dhyey-19@users.noreply.github.com>
2026-03-23 10:50:19 +0530

    Time: 6 ms (14.23%), Space: 312 MB (53.91%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -1,22 +1,49 @@
+// class Solution {
+// public:
+//     ListNode* deleteMiddle(ListNode* head) {
+//         // Edge case: only one node
+//         if (!head || !head->next) return nullptr;
+
+//         ListNode* slow = head;
+//         ListNode* fast = head;
+//         ListNode* prev = nullptr;
+
+//         while (fast && fast->next) {
+//             prev = slow;
+//             slow = slow->next;
+//             fast = fast->next->next;
+//         }
+
+//         // Delete middle node
+//         prev->next = slow->next;
+
+//         return head;
+//     }
+// };
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        // Edge case: only one node
-        if (!head || !head->next) return nullptr;
+        int len = 0;
+        for (ListNode* p = head; p != nullptr; p = p->next) ++len;
 
-        ListNode* slow = head;
-        ListNode* fast = head;
-        ListNode* prev = nullptr;
+        if (len / 2 == 0) return head->next;
+        
+        ListNode* p = head;
+        for (int i = 0; i < len / 2 - 1; ++i) p = p->next;
 
-        while (fast && fast->next) {
-            prev = slow;
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-
-        // Delete middle node
-        prev->next = slow->next;
-
+        p->next = p->next->next;
         return head;
     }
 };
