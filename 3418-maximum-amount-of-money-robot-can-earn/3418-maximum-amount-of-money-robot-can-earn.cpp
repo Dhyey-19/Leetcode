907f1831b1aef159be9659ec4038fdc2ca4efb04
SHAH DHYEY PARAS <103097959+Dhyey-19@users.noreply.github.com>
2026-04-02 17:31:04 +0530

    Time: 603 ms (36.17%), Space: 276 MB (46.81%) - LeetHub

--- 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.cpp
+++ 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.cpp
@@ -1,42 +1,35 @@
 class Solution {
 public:
-    int m, n;
-    vector<vector<vector<int>>> dp;
-
-    int solve(vector<vector<int>>& coins, int i, int j, int skips) {
-        if(i >= m || j >= n) return INT_MIN;
-
-        if(dp[i][j][skips] != INT_MIN) return dp[i][j][skips];
-
-        if(i == m - 1 && j == n - 1) {
-            if(coins[i][j] < 0 && skips > 0) {
-                return dp[i][j][skips] = 0;
-            }
-            return dp[i][j][skips] = coins[i][j];
-        }
-
-        // Take
-        int take = coins[i][j] + max(
-            solve(coins, i+1, j, skips),
-            solve(coins, i, j+1, skips)
-        );
-
-        // Skip
-        int skip = INT_MIN;
-        if(coins[i][j] < 0 && skips > 0) {
-            skip = max(
-                solve(coins, i+1, j, skips-1),
-                solve(coins, i, j+1, skips-1)
-            );
-        }
-
-        return dp[i][j][skips] = max(take, skip);
-    }
-
     int maximumAmount(vector<vector<int>>& coins) {
-        m = coins.size();
-        n = coins[0].size();
-        dp.assign(m, vector<vector<int>>(n, vector<int>(3, INT_MIN)));
-        return solve(coins, 0, 0, 2);
+        int m = coins.size(), n = coins[0].size();
+
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(3, INT_MIN)));
+
+        for(int k = 0; k <= 2; k++) {
+            dp[m-1][n-1][k] = (coins[m-1][n-1] < 0 && k > 0) ? 0 : coins[m-1][n-1];
+        }
+
+        for(int i = m-1; i >= 0; i--) {
+            for(int j = n-1; j >= 0; j--) {
+                if(i == m-1 && j == n-1) continue;
+
+                for(int k = 0; k <= 2; k++) {
+                    int best = INT_MIN;
+
+                    // Take
+                    if(i+1 < m) best = max(best, coins[i][j] + dp[i+1][j][k]);
+                    if(j+1 < n) best = max(best, coins[i][j] + dp[i][j+1][k]);
+
+                    // Skip
+                    if(coins[i][j] < 0 && k > 0) {
+                        if(i+1 < m) best = max(best, dp[i+1][j][k-1]);
+                        if(j+1 < n) best = max(best, dp[i][j+1][k-1]);
+                    }
+
+                    dp[i][j][k] = best;
+                }
+            }
+        }
+        return dp[0][0][2];
     }
 };
