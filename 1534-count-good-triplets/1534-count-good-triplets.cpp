395dff362c4291adf772b4d25f48df442deff9fb
SHAH DHYEY PARAS <103097959+Dhyey-19@users.noreply.github.com>
2026-04-08 17:18:43 +0530

    Time: 5 ms (81.15%), Space: 11.1 MB (47.5%) - LeetHub

--- 1534-count-good-triplets/1534-count-good-triplets.cpp
+++ 1534-count-good-triplets/1534-count-good-triplets.cpp
@@ -4,14 +4,14 @@ public:
         int cnt = 0;
         for(int i = 0; i < arr.size() - 2; i++) {
             for(int j = i + 1; j < arr.size() - 1; j++) {
-                for(int k = j + 1; k < arr.size(); k++) {
-                    if(abs(arr[i] - arr[j]) <= a &&
-                       abs(arr[j] - arr[k]) <= b &&
-                       abs(arr[i] - arr[k]) <= c)
-                    {
-                        cnt++;
-                    }
-                }
+                if(abs(arr[i] - arr[j]) <= a) {
+                    for(int k = j + 1; k < arr.size(); k++) {
+                        if(abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c)
+                        {
+                            cnt++;
+                        }
+                    }                
+                }   
             }
         }    
         return cnt;
