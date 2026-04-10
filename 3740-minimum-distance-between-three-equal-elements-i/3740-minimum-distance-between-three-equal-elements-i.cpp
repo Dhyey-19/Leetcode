02bd609e81ddea969a4f6df6360cba823776e134
SHAH DHYEY PARAS <103097959+Dhyey-19@users.noreply.github.com>
2026-04-10 20:05:38 +0530

    Time: 4 ms (52.52%), Space: 28.8 MB (63.87%) - LeetHub

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int minimumDistance(vector<int>& nums) {
-        int minDistance = INT_MAX;
-        int n = nums.size();
-        for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
-                for(int k = j + 1; k < n; k++) {
-                    if(nums[i] == nums[j] && nums[i] == nums[k]) {
-                        minDistance = min(minDistance, abs(i-j) + abs(j-k) + abs(k-i));
-                    }
-                }
-            }
-        }    
-        return minDistance == INT_MAX ? -1 : minDistance;
+        int n = nums.size() ;
+        if (n <= 2) return -1 ;
+        int ans = INT_MAX ;
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++)
+                if (nums[i] == nums[j])
+                for (int k = j+1; k < n; k++)
+                    if (nums[j] == nums[k])
+                    ans = min(ans, 2*(k-i)) ;
+        }
+        if (ans == INT_MAX) return -1 ;
+        else return ans ;
     }
 };
