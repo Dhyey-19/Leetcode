dc9e4995bc4828e1193896b757004dcbac99bf2c
SHAH DHYEY PARAS <103097959+Dhyey-19@users.noreply.github.com>
2026-05-15 22:31:04 +0530

    Time: 3 ms (75.58%), Space: 19.2 MB (80.68%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,55 +1,56 @@
 class Solution {
 public:
-    bool isCycle(int node,
-                 vector<vector<int>>& adj,
-                 vector<bool>& visited,
-                 vector<bool>& recordPath) {
+    bool dfs(int node,
+             vector<vector<int>>& adj,
+             vector<int>& state) {
 
-        visited[node] = true;
-        recordPath[node] = true;
+        // Mark current node as VISITING
+        // 0 = unvisited
+        // 1 = visiting
+        // 2 = completely visited
+        state[node] = 1;
 
         for(int neighbour : adj[node]) {
 
-            if(!visited[neighbour]) {
-
-                if(isCycle(neighbour, adj, visited, recordPath)) {
-                    return true;
-                }
-
+            // If neighbour is already in current DFS path,
+            // then cycle exists
+            if(state[neighbour] == 1) {
+                return true;
             }
-            else if(recordPath[neighbour]) {
+
+            // If neighbour is unvisited,
+            // perform DFS on it
+            if(state[neighbour] == 0 &&
+               dfs(neighbour, adj, state)) {
                 return true;
             }
         }
 
-        recordPath[node] = false;
+        // Mark node as completely processed
+        state[node] = 2;
 
         return false;
     }
 
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+    bool canFinish(int numCourses,
+                   vector<vector<int>>& prerequisites) {
 
-        int V = numCourses;
-
-        vector<vector<int>> adj(V);
-
-        for(auto it : prerequisites) {
-            adj[it[1]].push_back(it[0]);
+        vector<vector<int>> adj(numCourses);
+        for(auto& p : prerequisites) {
+            adj[p[1]].push_back(p[0]);
         }
-
-        vector<bool> visited(V, false);
-        vector<bool> recordPath(V, false);
-
-        for(int i = 0; i < V; i++) {
-
-            if(!visited[i]) {
-
-                if(isCycle(i, adj, visited, recordPath)) {
+        vector<int> state(numCourses, 0);
+        for(int i = 0; i < numCourses; i++) {
+            // Start DFS only if node is unvisited
+            if(state[i] == 0) {
+                // If cycle found,
+                // courses cannot be completed
+                if(dfs(i, adj, state)) {
                     return false;
                 }
             }
         }
-
+        // No cycle found
         return true;
     }
 };
