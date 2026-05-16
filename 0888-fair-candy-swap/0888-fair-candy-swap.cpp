f03f42362fb1ff04f4ec8b0315f3135b35c639a7
SHAH DHYEY PARAS <103097959+Dhyey-19@users.noreply.github.com>
2026-05-16 14:50:17 +0530

    Time: 35 ms (37.74%), Space: 55.3 MB (5.32%) - LeetHub

---
+++ 0888-fair-candy-swap/0888-fair-candy-swap.cpp
@@ -0,0 +1,17 @@
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> fairCandySwap(vector<int> A, vector<int> B) {
+        int dif = (accumulate(A.begin(), A.end(), 0) - accumulate(B.begin(), B.end(), 0)) / 2;
+        unordered_set<int> S(A.begin(), A.end());
+        for (int b : B) {
+            if (S.count(b + dif)) {
+                return {b + dif, b};
+            }
+        }
+        return {};
+    }
+};
