47b44a79bfefaea59644e1b6a59dfa843c12510c
SHAH DHYEY PARAS <103097959+Dhyey-19@users.noreply.github.com>
2026-05-18 16:35:26 +0530

    Time: 3 ms (84.52%), Space: 75.9 MB (89.23%) - LeetHub

--- 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     bool canArrange(vector<int>& arr, int k) {
-        unordered_map<int,int> freq;
-
-        for(int num : arr) {
-            int rem = ((num % k) + k) % k;
+        vector<int> freq(k, 0);
+        for (int num : arr) {
+            int rem = (num % k + k) % k;
             freq[rem]++;
         }
-
-        if(freq[0] % 2 != 0)
+        if (freq[0] % 2 != 0)
             return false;
 
-        for(int r = 1; r < k; r++) {
-            if(r == k - r) {
-                if(freq[r] % 2 != 0)
-                    return false;
-            }
-            else {
-                if(freq[r] != freq[k-r])
-                    return false;
-            }
+        for (int i = 1; i <= k / 2; i++) {
+            if (freq[i] != freq[k - i])
+                return false;
         }
+        if (k % 2 == 0 && freq[k / 2] % 2 != 0)
+            return false;
 
         return true;
     }
