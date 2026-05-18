61480b137d93928bd3797b07ddd0263ab7ce8990
SHAH DHYEY PARAS <103097959+Dhyey-19@users.noreply.github.com>
2026-05-18 16:25:24 +0530

    Time: 0 ms (100%), Space: 9.6 MB (77.31%) - LeetHub

---
+++ 1496-path-crossing/1496-path-crossing.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPathCrossing(string path) {
+        int x = 0, y = 0;
+
+        set<pair<int,int>> visited;
+        visited.insert({0,0});
+
+        for(char ch : path) {
+            if(ch == 'N') y++;
+            else if(ch == 'S') y--;
+            else if(ch == 'E') x++;
+            else x--;
+
+            if(visited.count({x,y}))
+                return true;
+
+            visited.insert({x,y});
+        }
+
+        return false;
+    }
+};
