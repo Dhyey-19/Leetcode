746294d9661856efa20492aad27e48698ed582a9
SHAH DHYEY PARAS <103097959+Dhyey-19@users.noreply.github.com>
2026-05-28 19:20:14 +0530

    Time: 0 ms (100%), Space: 14.2 MB (41.41%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     void findCombination(int idx, int target, vector<int>& arr,
                          vector<vector<int>>& ans, vector<int>& ds) {
+        // if all elements are processed, check if target is acheived. if yes, store ans
         if (idx == arr.size()) {
             if (target == 0) {
                 ans.push_back(ds);
@@ -9,12 +10,15 @@ public:
             return;
         }
 
+        // if curr ele is <= target, add it in ds
         if(arr[idx] <= target) {
             ds.push_back(arr[idx]);
+            // recursive call for target - arr[idx] - to acheive the remaining target value
             findCombination(idx, target - arr[idx], arr, ans, ds);
+            // backtrack
             ds.pop_back();
         }
-
+        // if ele > target, skip it with recursive call of idx + 1
         findCombination(idx + 1, target, arr, ans, ds);
     }
 
