459cc1fb63ff01a1fa8fc88901a511e9a9a386bd
SHAH DHYEY PARAS <103097959+Dhyey-19@users.noreply.github.com>
2026-05-29 21:33:55 +0530

    Time: 0 ms (100%), Space: 7.7 MB (89.21%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isSameAfterReversals(int num) {
-        return !(num % 10 == 0) || num == 0;    
+        // return true if (num is 0) or (last digit is not 0)
+        return num % 10 || num == 0;
     }
 };
