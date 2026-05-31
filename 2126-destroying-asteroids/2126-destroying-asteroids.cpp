a49aaa5c25570d044acf9dd1ce758a5e148dea18
SHAH DHYEY PARAS <103097959+Dhyey-19@users.noreply.github.com>
2026-05-31 07:59:20 +0530

    Time: 11 ms (98.69%), Space: 140.6 MB (5.11%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.cpp
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
     bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
-        sort(asteroids.begin(), asteroids.end());
+        // sort(asteroids.begin(), asteroids.end());
 
-        long long currMass = mass;
+        // long long currMass = mass;
 
-        for (int asteroid : asteroids) {
-            if (currMass < asteroid) {
-                return false;
+        // for (int asteroid : asteroids) {
+        //     if (currMass < asteroid) {
+        //         return false;
+        //     }
+        //     currMass += asteroid;
+        // }
+
+        // return true;
+
+        // Counting Sort - O(n + 100001)
+        vector<int> freq(100001);
+        for (int x : asteroids)
+            freq[x]++;
+
+        long long curr = mass;
+        for (int x = 1; x <= 100000; x++) {
+            while (freq[x]--) {
+                if (curr < x)
+                    return false;
+                curr += x;
             }
-            currMass += asteroid;
         }
 
         return true;
