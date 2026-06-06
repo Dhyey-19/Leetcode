a9f9025fa457c17bd018ce2f19ed4f14f56de84f
SHAH DHYEY PARAS <103097959+Dhyey-19@users.noreply.github.com>
2026-06-06 19:23:31 +0530

    Time: 524 ms (41.03%), Space: 240 MB (72.82%) - LeetHub

---
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int mergeSort(vector<int>& nums, int l, int r) {
+        if(l >= r) return 0;
+
+        int mid = l + (r - l) / 2;
+
+        int cnt = 0;
+
+        cnt += mergeSort(nums, l, mid);
+        cnt += mergeSort(nums, mid + 1, r);
+
+        int j = mid + 1;
+
+        for(int i = l; i <= mid; i++) {
+            while(j <= r && nums[i] > 2LL * nums[j]) {
+                j++;
+            }
+            cnt += j - (mid + 1);
+        }
+
+        vector<int> temp;
+        int left = l;
+        int right = mid + 1;
+
+        while(left <= mid && right <= r) {
+            if(nums[left] <= nums[right])
+                temp.push_back(nums[left++]);
+            else
+                temp.push_back(nums[right++]);
+        }
+
+        while(left <= mid)
+            temp.push_back(nums[left++]);
+
+        while(right <= r)
+            temp.push_back(nums[right++]);
+
+        for(int i = l; i <= r; i++) {
+            nums[i] = temp[i - l];
+        }
+
+        return cnt;
+    }
+
+    int reversePairs(vector<int>& nums) {
+        return mergeSort(nums, 0, nums.size() - 1);
+    }
+};
